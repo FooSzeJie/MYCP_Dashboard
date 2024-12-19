f5c0c1fe2e51bfba93a8e20cfdf3f4b6a6a5d243
Jayden <jaydenfoo02@gmail.com>
2024-12-19 14:55:12 +0800

    Improve Route

--- src/App.js
+++ src/App.js
@@ -4,12 +4,8 @@ import { CssBaseline, ThemeProvider } from "@mui/material";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Topbar from "./scenes/global/Topbar";
 import Sidebar from "./scenes/global/Sidebar";
-import Dashboard from "./scenes/dashboard";
-import LocalAuthority from "./scenes/local Authority/manageLocalAuthority";
-import AddLocalAuthority from "./scenes/local Authority/addLocalAuthority";
-import EditLocalAuthority from "./scenes/local Authority/edit";
-import Users from "./scenes/users/manageUser";
-import EditUser from "./scenes/users/edit";
+
+import AdminRoutes from "./routes/admin_route";
 import Login from "./scenes/login";
 
 function App() {
@@ -58,24 +54,9 @@ function App() {
         {isAuthenticated ? (
           <div className="app">
             <Sidebar />
-
             <main className="content">
               <Topbar onLogout={handleLogout} />
-              <Routes>
-                <Route path="/" element={<Dashboard />} />
-                <Route path="/local_authority" element={<LocalAuthority />} />
-                <Route
-                  path="/local_authority/add"
-                  element={<AddLocalAuthority />}
-                />
-                <Route
-                  path="/local_authority/edit/:lid"
-                  element={<EditLocalAuthority />}
-                />
-                <Route path="/users" element={<Users />} />
-                <Route path="/users/edit/:uid" element={<EditUser />} />
-                <Route path="*" element={<Navigate to="/" replace />} />
-              </Routes>
+              <AdminRoutes />
             </main>
           </div>
         ) : (
---
+++ src/routes/admin_route.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Dashboard from "../scenes/dashboard";
+import LocalAuthority from "../scenes/local Authority/manageLocalAuthority";
+import AddLocalAuthority from "../scenes/local Authority/addLocalAuthority";
+import EditLocalAuthority from "../scenes/local Authority/edit";
+import Users from "../scenes/users/manageUser";
+import EditUser from "../scenes/users/edit";
+
+const AdminRoutes = () => (
+  <Routes>
+    <Route path="/" element={<Dashboard />} />
+    <Route path="/local_authority" element={<LocalAuthority />} />
+    <Route path="/local_authority/add" element={<AddLocalAuthority />} />
+    <Route path="/local_authority/edit/:lid" element={<EditLocalAuthority />} />
+    <Route path="/users" element={<Users />} />
+    <Route path="/users/edit/:uid" element={<EditUser />} />
+    <Route path="*" element={<Navigate to="/" replace />} />
+  </Routes>
+);
+
+export default AdminRoutes;
